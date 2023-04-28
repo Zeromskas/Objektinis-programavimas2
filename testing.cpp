02f7c205d8b7d3f7abb55b7ba0ca9bf3698a84e5
Pranas Zeromskas <76076765+Zeromskas@users.noreply.github.com>
2023-04-28 11:43:54 +0300

    v1.2

--- testing.cpp
+++ testing.cpp
@@ -3,7 +3,7 @@
 struct Time
 {
     string part;
-    double duration[5]={0};
+    double duration[5] = {0};
 };
 
 int main()
@@ -13,27 +13,32 @@ int main()
     std::chrono::high_resolution_clock::time_point start_time, end_time;
     duration<double> dur;
 
-    //string filenames[5]{"studentai10M.txt", "studentai1M.txt", "studentai100k.txt", "studentai10k.txt", "studentai1k.txt"};
+    // string filenames[5]{"studentai10M.txt", "studentai1M.txt", "studentai100k.txt", "studentai10k.txt", "studentai1k.txt"};
     string filenames[1]{"studentai1M.txt"};
 
     Time times[5];
-    times[0].part="Nuskaitymas", times[1].part="Rikiavimas", times[2].part="Dalinimas", times[3].part="Išvedimas", times[4].part="Viso";
+    times[0].part = "Nuskaitymas", times[1].part = "Rikiavimas", times[2].part = "Dalinimas", times[3].part = "Išvedimas", times[4].part = "Viso";
 
     int i;
-    for(auto filename : filenames)
+    for (auto filename : filenames)
     {
-        if(filename == "studentai1k.txt") i=0;
-        else if(filename == "studentai10k.txt") i=1;
-        else if(filename == "studentai100k.txt") i=2;
-        else if(filename == "studentai1M.txt") i=3;
-        else if(filename == "studentai10M.txt") i=4;
+        if (filename == "studentai1k.txt")
+            i = 0;
+        else if (filename == "studentai10k.txt")
+            i = 1;
+        else if (filename == "studentai100k.txt")
+            i = 2;
+        else if (filename == "studentai1M.txt")
+            i = 3;
+        else if (filename == "studentai10M.txt")
+            i = 4;
 
         total_start = high_resolution_clock::now();
         start_time = high_resolution_clock::now();
         readFile(students, filename);
         end_time = high_resolution_clock::now();
         dur = end_time - start_time;
-        times[0].duration[i]=dur.count();
+        times[0].duration[i] = dur.count();
         start_time = high_resolution_clock::now();
         sort(students.begin(), students.end(), compareName);
         end_time = high_resolution_clock::now();
@@ -45,27 +50,24 @@ int main()
         dur = end_time - start_time;
         times[2].duration[i] = dur.count();
         start_time = high_resolution_clock::now();
-        print(studPass, "studPass.txt");
-        studPass.clear();
-        print(students, "studFail.txt");
-        students.clear();
-        end_time= high_resolution_clock::now();
+        printBoth(studPass, "studPass.txt", students, "studFail.txt");
+        end_time = high_resolution_clock::now();
         dur = end_time - start_time;
         times[3].duration[i] = dur.count();
-        total_end= high_resolution_clock::now();
+        total_end = high_resolution_clock::now();
         dur = total_end - total_start;
         times[4].duration[i] = dur.count();
     }
 
-    cout<<"|                  | **1k**    | **10k**  | **100k** | **1M** | **10M** |"<<endl;
-    cout<<"|------------------|-----------|----------|----------|--------|---------|"<<endl;
-    
-    for(int i = 0; i < 5; i++)
+    cout << "|                  | **1k**    | **10k**  | **100k** | **1M** | **10M** |" << endl;
+    cout << "|------------------|-----------|----------|----------|--------|---------|" << endl;
+
+    for (int i = 0; i < 5; i++)
     {
-        cout << "| **"<<times[i].part<<"** |";
-        for(int j=0; j<5; j++)
-            cout << " _"<<fixed << setprecision(8-j) << times[i].duration[j] <<"_ |";
-        cout<<endl;
+        cout << "| **" << times[i].part << "** |";
+        for (int j = 0; j < 5; j++)
+            cout << " _" << fixed << setprecision(8 - j) << times[i].duration[j] << "_ |";
+        cout << endl;
     }
 
     return 0;
