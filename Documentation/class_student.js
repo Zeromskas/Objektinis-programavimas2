5eadf18dfb03d6b4bc26c5d3ec16dc979600dd7e
Pranas Zeromskas <76076765+Zeromskas@users.noreply.github.com>
2023-05-04 23:48:49 +0300

    added doxygen documentation

---
+++ Documentation/class_student.js
@@ -0,0 +1,20 @@
+var class_student =
+[
+    [ "Student", "class_student.html#aa7191d2188ddbb8d80eb92a8745ab4e5", null ],
+    [ "Student", "class_student.html#a742aed7d40ca0cb91552e43a672e368b", null ],
+    [ "Student", "class_student.html#a05b37ffb050ddb039db63a8764d790cb", null ],
+    [ "Student", "class_student.html#ad16f78f16c1eb0e3eee37809602c7701", null ],
+    [ "Student", "class_student.html#af9168cedbfa5565cf0b20c1a9d3f5c9d", null ],
+    [ "~Student", "class_student.html#a54a8ea060d6cd04222c3a2f89829f105", null ],
+    [ "countAverage", "class_student.html#aa4fcfe06b1b4ade56144fb3958d3d921", null ],
+    [ "countFinal", "class_student.html#adf5014fad35d69a0d95f39ac20f0782f", null ],
+    [ "countMedian", "class_student.html#a2bb42ec954de47125938106a346b85a5", null ],
+    [ "getAverage", "class_student.html#a759f352226098d424b5043db4ca5b608", null ],
+    [ "getMedian", "class_student.html#a71bef54ba231e563b8ca0205c8ecc728", null ],
+    [ "getName", "class_student.html#a1df6c28e6416e20d338b9ff11bdc1e04", null ],
+    [ "getSurname", "class_student.html#a785a71f1e09c7f2c142d5a775739a76a", null ],
+    [ "operator=", "class_student.html#ad05dd6148523a2a8b38128b60297b52a", null ],
+    [ "operator=", "class_student.html#a4e8f98630a250a691b385d1b640f863d", null ],
+    [ "operator<<", "class_student.html#a2ac679e8b73af17719cbb2620f23098b", null ],
+    [ "operator>>", "class_student.html#abfb8f423312cee036074bc708c24181a", null ]
+];
---
+++ Documentation/dir_e04ae2849dfa520b4ee2c9f7e9ff15de.js
@@ -0,0 +1,6 @@
+var dir_e04ae2849dfa520b4ee2c9f7e9ff15de =
+[
+    [ "functions.cpp", "functions_8cpp.html", "functions_8cpp" ],
+    [ "functions.h", "functions_8h.html", "functions_8h" ],
+    [ "main.cpp", "main_8cpp.html", "main_8cpp" ]
+];
